ead00f318a9d6501166d962cea70e181334e8662
Vicente <vicentemulero@hotmail.com>
2021-06-01 16:54:52 +0200

    Se modifica botón de añadir género

--- resources/views/genre/formCreateGenre.blade.php
+++ resources/views/genre/formCreateGenre.blade.php
@@ -29,7 +29,7 @@
                     <div class="form-group row mb-0">
                         <div class="col-md-8 offset-md-4">
                             <button type="submit" class="btn btn-primary">
-                                Crear género
+                                Añadir género
                             </button>
                         </div>
                     </div>
